fc3cb61490508e780ef18a4f8cf325f1a7434007
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2019-11-25 15:30:22 +0000

    Fixed css module

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-import Classes from './Layout.css';
+import styles from './layout.module.css';
 
 const layout = (props) => (
      <Aux>
           <div>Toolbar, SideDrawer, Backdrop</div>
-          <main className={Classes.content}>
+          <main className={styles["content"]}>
                {props.children}
           </main>
      </Aux>
