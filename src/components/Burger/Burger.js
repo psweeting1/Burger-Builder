bcce0d34d6b62cdcce314690bfe124d2e7ab534c
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-07-07 08:07:09 +0100

    Beginning of a dynamic burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import style from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+
+    //Keys returns an array which we can now map
+    const transformedIngredients = Object.keys(props.ingredients)
+        //igKey is the key in the map used in the array
+        .map(igKey => {
+            //Use the spread operator (...)
+            return [...Array(props.ingredients[igKey])]
+                // The _ is blank, don't care about that argument, but we care about index (i)
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />
+                });
+        });
      return (
           <div className={style.burger}>
                <BurgerIngredient type="bread-top"/>
-               <BurgerIngredient type="seeds1"/>
-               <BurgerIngredient type="cheese"/>
-               <BurgerIngredient type="meat"/>
+                {transformedIngredients}
                <BurgerIngredient type="bread-bottom"/>
           </div>
      );
--- src/hoc/Aux.js
+++ src/hoc/Aux.js
@@ -1,3 +1,4 @@
+// Higher Order Component
 const aux = (props) => props.children;
 
 export default aux;
