694db00aed4dcb534077f1147d5a212a31232050
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-07-08 08:49:14 +0100

    Created buildcontrols and buildcontrol for burger builder.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
 class BurgerBuilder extends Component {
 
     //This is an object, not an array, need to convert this using Object Mapping to an Array.
@@ -17,7 +19,7 @@ class BurgerBuilder extends Component {
           return (
                <Aux>
                     <Burger ingredients={this.state.ingredients}/>
-                    <div>Build Controls</div>
+                    <BuildControls/>
                </Aux>
           );
      }
