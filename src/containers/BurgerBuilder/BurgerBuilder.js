c24f67d322c6f4cf86cbd3d9b12c50b5ce0e4550
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-07-13 08:51:28 +0100

    Added an add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,8 +3,15 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
-class BurgerBuilder extends Component {
+//Global variable (Global variables are in capitals)
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
+class BurgerBuilder extends Component {
     //This is an object, not an array, need to convert this using Object Mapping to an Array.
     state = {
         ingredients: {
@@ -12,14 +19,30 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4.0
+    }
+
+    addIngredientHandler = (type) => {
+        //Yes you can do this in less lines, but for training purposes it's laid out simple.
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
 
      render() {
           return (
                <Aux>
                     <Burger ingredients={this.state.ingredients}/>
-                    <BuildControls/>
+                    <BuildControls
+                    ingredientAdded={this.addIngredientHandler}/>
                </Aux>
           );
      }
