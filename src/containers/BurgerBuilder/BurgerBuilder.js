84bb780662035663d0173c06d1d4868d944d7663
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-07-13 09:22:05 +0100

    Disabled element added for buttons and price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,12 +37,41 @@ class BurgerBuilder extends Component {
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     }
 
+    removeIngredientHandler = (type) => {
+        //Yes you can do this in less lines, but for training purposes it's laid out simple.
+        const oldCount = this.state.ingredients[type];
+        // Stops crashes if less than zero ingredients.
+        if(oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
      render() {
+        const disabledInfo = {
+            // This is a copy of the state for ingredients.
+            ...this.state.ingredients
+        }
+        for (let key in disabledInfo) {
+            // Will update disabledInfo state with true or false
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
           return (
                <Aux>
                     <Burger ingredients={this.state.ingredients}/>
                     <BuildControls
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    price={this.state.totalPrice}
+                    disabled={disabledInfo}/>
                </Aux>
           );
      }
