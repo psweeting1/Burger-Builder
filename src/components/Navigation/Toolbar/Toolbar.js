a05f24550ee6e78a54b509181b130487aa057fc1
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-11-10 09:45:16 +0000

    Adding in navigation menu

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Toolbar.module.css'
+
+
+const toolbar = (props) => (
+    <header className={styles.toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            <ul>...</ul>
+        </nav>
+    </header>
+)
+
+export default toolbar
