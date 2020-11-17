664526033bd8373f54bbfe59adecc8df932ac91c
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-11-17 09:20:04 +0000

    Created Navigation Components

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Toolbar.module.css'
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 
 const toolbar = (props) => (
     <header className={styles.toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
-            <ul>...</ul>
+            <NavigationItems/>
         </nav>
     </header>
 )
