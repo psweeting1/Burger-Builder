fe7cb3484a5bc09b742b2a2a1c35202eb04a2a05
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-11-27 08:11:45 +0000

    Fixing backdrop default true to false

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import SideDraw from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
