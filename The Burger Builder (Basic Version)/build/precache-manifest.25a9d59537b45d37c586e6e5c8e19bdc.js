e4a05ce0f0273121bf210858e8c5fd016a400a34
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-12-03 08:00:57 +0000

    Refactoring folders

---
+++ The Burger Builder (Basic Version)/build/precache-manifest.25a9d59537b45d37c586e6e5c8e19bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd98af9e29f15cc8548cec528661464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f232f33eebf1611174",
+    "url": "/static/css/main.ce3b43f9.chunk.css"
+  },
+  {
+    "revision": "39ffebed694972a05cd1",
+    "url": "/static/js/2.0fd36fbb.chunk.js"
+  },
+  {
+    "revision": "c8f232f33eebf1611174",
+    "url": "/static/js/main.88123cb8.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
