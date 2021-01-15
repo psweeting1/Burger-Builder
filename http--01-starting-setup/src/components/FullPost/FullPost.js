4f8a28ddee8bb4cba34f306ef7a7b263bd04d66f
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2021-01-15 09:51:10 +0000

    Pushing up axios work

---
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './FullPost.css';
+
+class FullPost extends Component {
+  state = {
+    loadedPost: null
+  }
+
+  componentDidUpdate() {
+    console.log(this.props.id + " props id")
+    console.log(this.state.loadedPost + " loadedPost")
+    if (this.props.id) {
+      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+          //Prevents infinite looping
+          axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+            .then(response => {
+              this.setState({loadedPost: response.data})
+            })
+          }
+        }
+      }
+
+  render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+          let post = <p style={{textAlign: 'center'}}>Loading</p>;
+        }
+        if (this.state.loadedPost) {
+          console.log(this.state.loadedPost.id + "id is")
+          console.log(this.state.loadedPost.userId + "user id is")
+          post = (
+            <div className="FullPost">
+              <h1>{this.state.loadedPost.title}</h1>
+              <p>{this.state.loadedPost.body}</p>
+              <div className="Edit">
+                  <button className="Delete">Delete</button>
+              </div>
+            </div>
+
+          );
+        }
+      return post;
+  }
+}
+
+export default FullPost;
