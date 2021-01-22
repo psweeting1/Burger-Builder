8bd48fdb695456d247a60cb89caaa661e9e5ca17
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2021-01-22 08:13:08 +0000

    Delete post

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios'
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -23,6 +24,8 @@ class Blog extends Component {
                 }
             })
             this.setState({posts: updatedPosts})
+        }).catch(error => {
+            this.setState({error: true})
         })
     }
 
@@ -31,13 +34,17 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-            key={post.id} 
-            title={post.title} 
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        console.log(this.state.error)
+        if (!this.state.error) {
+          posts = this.state.posts.map(post => {
+              return <Post 
+              key={post.id} 
+              title={post.title} 
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}/>
+          })
+        }
         return (
             <div>
                 <section className="Posts">
